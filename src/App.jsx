8f85f4991ab8d2bfbbc8c325b169f707d20f26ed
Max Shabalin <shabalinmaksimromanovich@gmail.com>
2023-01-03 23:19:19 +0300

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import {useDispatch, useSelector} from "react-redux";
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import {setUser} from "./redux/Slices/authSlice";
+import RegisterOrLoginPage from "./Pages/RegisterOrLoginPage";
+import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
 
 function App() {
   const count = useSelector(state => state.counter.value)
-  const auth = useSelector(state => state.counter.value)
   const dispatch = useDispatch()
   return (
     <div className="App">
-      <button onClick={() => dispatch(setUser({
-        name: 'max',
-        age: 20,
-      }))}>
-        +
-      </button>
+      <Routes>
+          <Route path={'/'} element={<RegisterOrLoginPage/>}/>
+          <Route path={'/login'} element={<LoginPage/>}/>
+          <Route path={'/registration'} element={<RegisterPage/>}/>
+      </Routes>
     </div>
+
   );
 }
 
